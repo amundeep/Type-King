f15dc7aea645148c161449765bf9586bcf68244a
Amundeep Singh <amundeep@gmail.com>
2014-06-27 02:29:51 -0700

    UI (Main Menu)
    Added functionality to “About” button

--- src/me/amundeep/typeking/MainMenu.java
+++ src/me/amundeep/typeking/MainMenu.java
@@ -2,7 +2,9 @@ package me.amundeep.typeking;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,7 +14,10 @@ import android.widget.ImageView;
 
 public class MainMenu extends Activity{
 
-	ImageView ivStart, ivInstructions, ivAbout;
+	Context context = this;
+	
+	
+	ImageView ivStart, ivInstructions, ivAbout, ivOptions;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +26,12 @@ public class MainMenu extends Activity{
 		
 		setContentView(R.layout.activity_mainmenu);
 
+		
+		
 		ivStart = (ImageView) findViewById(R.id.ivStart);
 		ivInstructions = (ImageView) findViewById(R.id.ivInstructions);
 		ivAbout = (ImageView) findViewById(R.id.ivAbout);
+		ivOptions = (ImageView) findViewById(R.id.ivOptions);
 		
 		ivStart.setOnClickListener(new OnClickListener(){
 
@@ -45,6 +53,34 @@ public class MainMenu extends Activity{
 		});
 		
 		ivAbout.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+				String VERSION = "n/a";
+				try {
+					VERSION = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
+				} catch (NameNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				AlertDialog alertDialog = new AlertDialog.Builder(context).create(); //Read Update
+				alertDialog.setTitle("About Type King");
+		        alertDialog.setMessage("Developed and Created by Amundeep Singh\n\nVersion: " + 
+		        					   VERSION + "\n\nKeyboard Key Icon by Arthur Shlain through TheNounProject");
+		        
+		        //Removed because ruined the dialog look with a default button there...
+//		        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Close",new DialogInterface.OnClickListener() {
+//			           public void onClick(DialogInterface dialog, int which) {}
+//			    });
+		        
+		        alertDialog.show();
+			}
+		});
+		
+		ivOptions.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
